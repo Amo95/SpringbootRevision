239dc0b4271fe5520d1b01c228350297b1e3e129
amo95 <jameaamo@gmail.com>
2022-12-28 14:18:53 +0000

    update endpoint controller

--- src/main/java/com/jeimseu/controller/CustomerEndpoint.java
+++ src/main/java/com/jeimseu/controller/CustomerEndpoint.java
@@ -2,11 +2,8 @@ package com.jeimseu.controller;
 
 import com.jeimseu.model.Customer;
 import com.jeimseu.service.CustomerService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,12 +17,32 @@ public class CustomerEndpoint {
     }
 
     @PostMapping("/save/customer")
-    public ResponseEntity<Customer> persistCustomerInfo(@RequestBody Customer customer){
-        return ResponseEntity.ok().body(customer);
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer persistCustomerInfo(@RequestBody Customer customer){
+        return customerService.persistUserData(customer);
     }
 
-    @GetMapping("/fetch/customer")
+    @GetMapping("/fetch/customers")
+    @ResponseStatus(HttpStatus.OK)
     public List<Customer> fetchAllCustomerInfo(){
-        return customerService.fetchAllCustomerInfo();
+        return customerService.fetchAllCustomersInfo();
+    }
+
+    @GetMapping("/fetch/customer/")
+    @ResponseStatus(HttpStatus.OK)
+    public Customer fetchCustomerById(@RequestParam Long id){
+        return customerService.fetchAllCustomerInfoById(id);
+    }
+
+    @DeleteMapping("/delete/customer")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public String deleteCustomer(@RequestParam Long id){
+        return customerService.deleteCustomerById(id);
+    }
+
+    @PutMapping("/update/customer")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer updateCustomer(@RequestParam Long id, @RequestBody Customer customer){
+        return customerService.updateCustomerInfoById(id, customer);
     }
 }
--- src/main/java/com/jeimseu/service/CustomerService.java
+++ src/main/java/com/jeimseu/service/CustomerService.java
@@ -2,6 +2,7 @@ package com.jeimseu.service;
 
 import com.jeimseu.model.Customer;
 import com.jeimseu.repository.CustomerRepository;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,11 +16,41 @@ public class CustomerService {
         this.customerRepository = customerRepository;
     }
 
-    public void persistUserData(){
-        customerRepository.save(new Customer());
+    public Customer persistUserData(Customer customer){
+        return customerRepository.save(customer);
     }
 
-    public List<Customer> fetchAllCustomerInfo(){
+    public List<Customer> fetchAllCustomersInfo(){
         return customerRepository.findAll();
+    }
+
+    public Customer fetchAllCustomerInfoById(Long id){
+        return customerRepository.findById(id).orElse(null);
+    }
+
+    public String deleteCustomerById(Long id){
+        String message;
+        try {
+            customerRepository.deleteById(id);
+            message = "Customer deleted successfully!";
+        } catch (EmptyResultDataAccessException exception) {
+            message = "id not found!";
+        }
+        return message;
+    }
+
+    public Customer updateCustomerInfoById(Long id, Customer customer){
+        return customerRepository.findById(id)
+                .stream()
+                .map(customer1 -> updateCustomer(customer, customer1))
+                .findFirst()
+                .orElse(null);
+    }
+
+    private Customer updateCustomer(Customer customer, Customer customer1) {
+        customer1.setAge(customer.getAge());
+        customer1.setEmail(customer.getEmail());
+        customer1.setName(customer.getName());
+        return customerRepository.save(customer1);
     }
 }
